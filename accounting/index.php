2665b7af6c8fae55574751dd40ffa642758a69e8
Inequable <37436938+Inequable@users.noreply.github.com>
2018-06-06 11:12:19 +0800

    Update index.php

--- accounting/index.php
+++ accounting/index.php
@@ -18,10 +18,10 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',True);
 
 // 定义应用目录
-define('APP_PATH','./Application/');
-define('JS_URL','/accounting/Public/js/');//JavaScript文件路径
-define('CSS_URL','/accounting/Public/style/');//style文件路径
-define('IMG_URL','/accounting/Public/images/');//images路径
+// define('APP_PATH','./Application/');
+// define('JS_URL','/accounting/Public/js/');//JavaScript文件路径
+// define('CSS_URL','/accounting/Public/style/');//style文件路径
+// define('IMG_URL','/accounting/Public/images/');//images路径
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
